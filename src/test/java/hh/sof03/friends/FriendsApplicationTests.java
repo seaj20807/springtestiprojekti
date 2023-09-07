54e2e7d44a1098f87e53e7e560eb6905e85277d8
Sampo Jaakkola <sampo.jaakkola@outlook.com>
2023-09-07 10:49:00 +0300

    testi

---
+++ src/test/java/hh/sof03/friends/FriendsApplicationTests.java
@@ -0,0 +1,13 @@
+package hh.sof03.friends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
